9602705388e0c2818f2a0ea6d97f7f715637a621
rowlandwanderi <rowlandswanderi@gmail.com>
2024-07-09 14:30:32 +0000

    updated cv and twitter links

--- src/sections/footer/data.js
+++ src/sections/footer/data.js
@@ -13,7 +13,7 @@ export const links = [
 
 
 export const socials = [
-    {id: 1, link: 'https://twitter.com/StoicMwas', icon: <AiOutlineTwitter/>},
+    {id: 1, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
     {id: 3, link: 'https://www.linkedin.com/in/rowland-mwangi-54ab901b3/', icon: <AiOutlineLinkedin/>},
     {id: 4, link: 'https://github.com/RowlandWanderi', icon: <AiFillGithub/>}
 ]
--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -6,7 +6,7 @@ import {AiFillGithub} from 'react-icons/ai'
 
 const data = [
 
-    {id: 2, link: 'https://twitter.com/StoicMwas', icon: <AiOutlineTwitter/>},
+    {id: 2, link: 'https://twitter.com/', icon: <AiOutlineTwitter/>},
     {id: 3, link: 'https://www.linkedin.com/in/rowland-mwangi-54ab901b3/', icon: <AiOutlineLinkedin/>},
     {id: 4, link: 'https://github.com/RowlandWanderi', icon: <AiFillGithub/>}
 ]
